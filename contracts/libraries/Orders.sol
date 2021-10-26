79f0adbe1e8b499684f9b2502abc5eb59ff622ff
FinagleLord <72840971+FinagleLord@users.noreply.github.com>
2021-10-25 19:42:52 -0500

    updated orders

--- contracts/libraries/Orders.sol
+++ contracts/libraries/Orders.sol
@@ -9,10 +9,11 @@ library Orders {
     struct Order {
         address maker;              // msg.sender, creator of order.
         address depositor;          // account that receives the bond.
-        address principal;          // principal used to buy bond.
+        uint256 amount;             // amount of principal being used to purchase bond.
         uint256 maxBondPrice;       // max price user's willing to pay denominated in principal.
+        uint256 BID;                // bond's unique identifier
+        address FEO;                // frontend operator
         uint256 deadline;           // time when order can't be filled after.
-        // EIP-712
         uint8 v;
         bytes32 r;
         bytes32 s;
@@ -25,13 +26,17 @@ library Orders {
                     ORDER_TYPEHASH,
                     order.maker,
                     order.depositor,
+                    order.amount,
                     order.principal,
                     order.maxBondPrice,
+                    order.BID,
+                    order.FEO,
                     order.deadline
                 )
             );
     }
 
+    // TODO add olympus related require statements
     function validate(Order memory order) internal {
         require(order.maker != address(0), "invalid-maker");
         require(order.depositor != address(0), "invalid-from-token");
